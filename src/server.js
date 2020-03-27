21bf28cae4eb76adfb3b741281d45ec193877e91
MediocreIan <idrews094@gmail.com>
2020-03-27 15:00:17 -0500

    deployment ready

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
+app.listen(PORT, () => {});
